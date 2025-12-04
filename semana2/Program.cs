c14af0beef9d7303f9bf718ba4ec2182137b7f09
Cristian Chiquimba <cd.chiquimbam@uea.edu.ec>
2025-12-04 15:54:26 -0500

    Agrago clases Rectangulo, Cuadrado y Programa actualizado,

--- semana2/Program.cs
+++ semana2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("CRISTIAN CHIQUIMBA");
+Console.WriteLine();
+
+// Crear un rectángulo con base 8 y altura 9
+Rectangulo rectangulo = new Rectangulo(8, 9);
+
+// Encabezado de tabla
+Console.WriteLine("Rectangulo       | Valor");
+Console.WriteLine("-----------------|-----------");
+
+// Valores del rectángulo
+Console.WriteLine("Base             | " + rectangulo.Base);
+Console.WriteLine("Altura           | " + rectangulo.Altura);
+Console.WriteLine("Área             | " + rectangulo.CalcularArea());
+Console.WriteLine("Perímetro        | " + rectangulo.CalcularPerimetro());
+
+Console.WriteLine();
